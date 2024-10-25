e8c92db0d3854ce607ba52bdf3d1ce0df75f432e
thiagogab <145212613+ThiagoGabriel001@users.noreply.github.com>
2024-10-24 23:59:01 -0300

    Add files via upload

---
+++ teste4.py
@@ -0,0 +1,5 @@
+# A) Esta é uma sequência de números ímpares.
+# B) Esta é uma sequência de potências de 2.
+# C) Esta é uma sequência de quadrados perfeitos.
+# D) Esta é uma sequência de quadrados de números pares.
+# E) Esta é a sequência de Fibonacci, onde cada número é a soma dos dois anteriores.
